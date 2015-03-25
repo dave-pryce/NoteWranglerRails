b70538ae5d2545d00e06a83fb21a4cd8631237b2
David Pryce <dpryce@seek.com.au>
2015-03-26 08:32:30 +1100

    CRUD note functionality all working

--- app/assets/javascripts/controllers/NotesCreateController.js
+++ app/assets/javascripts/controllers/NotesCreateController.js
@@ -1,11 +1,15 @@
-angular.module('NoteWrangler').controller('NotesCreateController', function($scope, note){
+angular.module('NoteWrangler').controller('NotesCreateController', function($scope, note, $location){
 	$scope.note = new note();
+	$scope.submitting = false;
 
 	$scope.saveNote = function(note){
-		note.$save()
+		$scope.submitting = true;
+		// then only if successful finally finished could be successful or not.
+		note.$save().then(function(){
+			$location.path("/notes");
+		}).finally(function(){
+			$scope.submitting = false;
+		});
 	}
-
-
-
 
 });
