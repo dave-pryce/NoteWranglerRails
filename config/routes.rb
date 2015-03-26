018dd8c84370236b9d159dfee4015a725082ec34
David Pryce <dpryce@seek.com.au>
2015-03-27 09:25:18 +1100

    User model and controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
  resources :notes, :defaults => {:format => :json}
 
  root to: 'notes#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326213914) do
+ActiveRecord::Schema.define(version: 20150326222237) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150326213914) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "site"
+    t.string   "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
