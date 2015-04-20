eb6dce9357536412db617f4e5ee74648e6c7ffed
David Pryce <dpryce@seek.com.au>
2015-04-21 08:44:16 +1000

    back end validation added to notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,17 @@
 class Note < ActiveRecord::Base
+	
+	# Relationships
 	belongs_to :category
 	belongs_to :user
 
-	# include user and categry as part of JSON
+	# Validation
+	validates :title, presence: true 
+	validates :description, presence: true 
+	validates :content, presence: true 
+	validates :user_id, presence: true 
+	validates :category_id, presence: true
+
+	# include user and categry as part of note JSON
 	def as_json(options={})
     super(:include => [:user,:category])
    	end
