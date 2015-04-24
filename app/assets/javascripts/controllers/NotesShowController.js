6387919aa1e6c39d02c8a46ff67092bd34d1f764
David Pryce <dpryce@seek.com.au>
2015-04-24 10:39:46 +1000

    clean up

--- app/assets/javascripts/controllers/NotesShowController.js
+++ app/assets/javascripts/controllers/NotesShowController.js
@@ -1,27 +1,15 @@
 angular.module('NoteWrangler').controller('NotesShowController', function(Note, $scope, $routeParams, $location){
 	$scope.note = Note.get({id: $routeParams.id});
 
-	// querying from category and user services
-	// Single user
-	//var noteobj = $scope.note;
-	//console.log(noteobj);
-	//console.log(user_id);
-	//$scope.user = User.get({id: '$scope.note.user_id'});
-	//$scope.category = Category.get({id: 7});
-	
-	// all users and categories
-	//$scope.users = User.query();
-	//$scope.categories = Category.query();
-
-	//console.log($scope.note);
-	//console.log($scope.user);
-	//console.log($scope.category);
-	
 	$scope.deleteNote = function(note){
-		note.$remove().then(function(){
-			$location.path("/notes/")
 
+	//	var yesDelete = confirm("Are you sure?");
+	//		if (yesDelete == true){
+
+				note.$remove().then(function(){
+				$location.path("/notes/")
+	//		};
+	//		else {};
 		});
 	}
-
 });
