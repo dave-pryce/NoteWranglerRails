f4783c6c1b5dfe05606d303c084d006fc343e932
David Pryce <dpryce@seek.com.au>
2015-04-27 08:17:21 +1000

    tidying up rails note controller
    removing unused code.

--- app/assets/javascripts/controllers/NotesShowController.js
+++ app/assets/javascripts/controllers/NotesShowController.js
@@ -2,14 +2,8 @@ angular.module('NoteWrangler').controller('NotesShowController', function(Note, 
 	$scope.note = Note.get({id: $routeParams.id});
 
 	$scope.deleteNote = function(note){
-
-	//	var yesDelete = confirm("Are you sure?");
-	//		if (yesDelete == true){
-
 				note.$remove().then(function(){
 				$location.path("/notes/")
-	//		};
-	//		else {};
 		});
 	}
 });
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -15,51 +15,34 @@ class NotesController < ApplicationController
     end
   end
 
-  # GET /notes/new
-  def new
-    @note = Note.new
-  end
-
-  # GET /notes/1/edit
-  def edit
-  end
-
-  # POST /notes
   # POST /notes.json
   def create
     @note = Note.new(note_params)
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
         format.json { render json: @note }
       else
-        format.html { render :new }
         format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /notes/1
   # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
         format.json { render json: @note }
       else
-        format.html { render :edit }
         format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
     @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
