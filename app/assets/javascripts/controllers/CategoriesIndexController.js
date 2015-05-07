b31b1a49d38c08387f843678d2418e3196e4ae06
David Pryce <dpryce@seek.com.au>
2015-05-08 08:43:22 +1000

    issues with multiple delete tracking

--- app/assets/javascripts/controllers/CategoriesIndexController.js
+++ app/assets/javascripts/controllers/CategoriesIndexController.js
@@ -7,29 +7,33 @@ angular.module('NoteWrangler').controller('CategoriesIndexController', function(
 		// add flagged to delete property to category
 		category['flaggedToDelete'] = true;
 		//console.log($scope.categories)
-		var index = $scope.categories.indexOf(category);
-		console.log(index);
+		//var index = $scope.categories.indexOf(category);
+		//console.log(index);
 		
-
+		//var categoryTimeOut = category.id
+		//console.log(categoryTimeOut)
 		//time out delete----------------------
-		timeout = $timeout(function(){
+		// define unique reference for the time out for this category
+		     $scope.cTimeOut = toString(category.id + category.name);
+		     console.log($scope.cTimeOut);
+		     $scope.cTimeOut = $timeout(function(){
 				category.$remove().then(function(){
-				$scope.categories.splice(index,1)
-				console.log($scope.categories)
+				$scope.categories.splice($scope.categories.indexOf(category),1)
+				//console.log($scope.categories)
 				});
 				}, 6000);
-		//-
+
 
 		};
 
 		// final delete
 		$scope.deleteCategory = function(category){
 		// Cancel timeout
-		$timeout.cancel(timeout);
+		$timeout.cancel($scope.cTimeOut);
 		// then delete
 		//console.log(category.id + "Deleted")
 			category.$remove().then(function(){
-				$location.path("/categories/")
+				$scope.categories.splice($scope.categories.indexOf(category),1)
 		});
 		};
 
@@ -37,9 +41,9 @@ angular.module('NoteWrangler').controller('CategoriesIndexController', function(
 		$scope.undoDelete = function(category){
 		// remove flagged to delete property from category
 		delete category.flaggedToDelete;
-		console.log($scope.categories);
+		//console.log($scope.categories);
 		// cancel timeout
-		$timeout.cancel(timeout);
+		$timeout.cancel($scope.cTimeOut);
 		};
 		
 	});
