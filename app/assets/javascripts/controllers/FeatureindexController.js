e739f27fc9fa08c6c25e902a9cfe4b5ed098027c
David Pryce <dpryce@seek.com.au>
2015-05-14 08:31:12 +1000

    ng-repeat to access label names now working.

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -1,16 +1,5 @@
 angular.module('NoteWrangler').controller('FeatureindexController', function(Feature, $scope){
 
-
 // get all data from githubIssue factory
  $scope.issues = Feature.query();
- $scope.labels = [];
- //console.log(Feature.query());
-
-	angular.forEach($scope.issues, function(issue, index){
-		angular.forEach(this.labels, function(label,index){
-	$scope.labels.push(label.name);
-   });
-  });
-
-console.log($scope.labels);
 });
