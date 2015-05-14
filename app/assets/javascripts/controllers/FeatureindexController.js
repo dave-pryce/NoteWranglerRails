03d8cbffbd45ca65c558ad9bd7f92cb72527dfc7
David Pryce <dpryce@seek.com.au>
2015-05-15 09:58:22 +1000

    playing with label filter.

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -2,5 +2,6 @@ angular.module('NoteWrangler').controller('FeatureindexController', function(Fea
 
 // get all data from githubIssue factory
 $scope.issues = Feature.query();
+console.log($scope.issues);
 
 });
