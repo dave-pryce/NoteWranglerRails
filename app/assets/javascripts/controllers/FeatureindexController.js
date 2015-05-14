37dcac3fdb74efa6056ae3b14298ae1395df22f2
David Pryce <dpryce@seek.com.au>
2015-05-15 09:16:53 +1000

    loading bars hidden using $resolved

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -1,5 +1,6 @@
 angular.module('NoteWrangler').controller('FeatureindexController', function(Feature, $scope){
 
 // get all data from githubIssue factory
- $scope.issues = Feature.query();
+$scope.issues = Feature.query();
+
 });
