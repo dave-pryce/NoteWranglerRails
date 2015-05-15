3180ff4ad2a395ef28ad5d1a59eb1a7e02d8906a
David Pryce <dpryce@seek.com.au>
2015-05-15 13:42:49 +1000

    basic search working on features

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -2,6 +2,7 @@ angular.module('NoteWrangler').controller('FeatureindexController', function(Fea
 
 // get all data from githubIssue factory
 $scope.issues = Feature.query();
-console.log($scope.issues);
+//console.log($scope.issues);
+$scope.search = {};
 
 });
