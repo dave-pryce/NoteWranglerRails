bc2fd0feba577925b20c1278e96719ed05418ec1
David Pryce <dpryce@seek.com.au>
2015-05-26 15:42:58 +1000

    updated foreach

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -1,15 +1,33 @@
 angular.module('NoteWrangler').controller('FeatureindexController', function(Feature, $scope){
 
 // get all data from githubIssue factory
-$scope.issues = Feature.query();
-//console.log($scope.issues);
-$scope.search = {};
+	$scope.issues = Feature.query();
+	//console.log($scope.issues);
+	$scope.search = {};
 
-$scope.isDP = function(label){
-	//console.log(label);
-	if (label.name == "DP") {
-	return true
+	$scope.isDP = function(label){
+		//console.log(label);
+		if (label.name == "DP") {
+		return true
+		};
 	};
-};
+
+	$scope.labelValue = function (){
+	angular.forEach($scope.issues, function(value,key)
+		{
+		// console.log(value.labels);
+		// return value.labels;
+
+		 angular.forEach(value.labels,function(value,key){
+		 	console.log(value.name);
+
+		 });
+
+
+		});
+   };
+
+
+
 
 });
