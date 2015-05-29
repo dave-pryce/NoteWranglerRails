3464e8ca0657ecf97811f1dae0446cac29e1e9c3
David Pryce <dpryce@seek.com.au>
2015-05-29 14:59:47 +1000

    one time binding
    Function being triggered twice.

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -2,30 +2,34 @@ angular.module('NoteWrangler').controller('FeatureindexController', function(Fea
 
 // get all data from githubIssue factory
 	$scope.issues = Feature.query();
-	//console.log($scope.issues);
+	console.log($scope.issues);
+	
 	$scope.search = {};
 
 	$scope.isDP = function(label){
-		//console.log(label);
+		console.log(label.name);
 		if (label.name == "DP") {return true};
 	};
 
 
-	$scope.labelValue = function (issuelabels){
+
+	//$scope.issueNo = function(issue){
+	//	console.log(issue.number + " " + issue.title);
+		//return issue.number + " " + issue.title;
+		//return true;
+	//};
+
+
+
+	$scope.labelValue = function (issuelabels){ 
 	//console.log(issuelabels);
 	angular.forEach(issuelabels, function(value,key){
- 		  
-		  if (value.name == "DP") {
+		//console.log(issue.number + ' ' + value.name);
+ 		  if (value.name == "DP") {
 			//console.log(value.name);
 		  	return true;
-		    };
-
-	//	  if (value.name == "STB") {
-		  //console.log("STB");
-		  //return "STB";
-	//	  };
-		  
-	});
+		   };
+		});
 	};
 
 
