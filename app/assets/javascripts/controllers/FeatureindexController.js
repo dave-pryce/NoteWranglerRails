2d357003c2bf6162279f2b7d1f91e8a6e18bfc42
dave-pryce <dp@Davids-MacBook-Pro.local>
2015-06-03 13:28:39 +1000

    basic filtering using search working.

--- app/assets/javascripts/controllers/FeatureindexController.js
+++ app/assets/javascripts/controllers/FeatureindexController.js
@@ -6,14 +6,14 @@ angular.module('NoteWrangler').controller('FeatureindexController', function(Fea
 	
 	$scope.search = {};
 
+	// hightlight DP label
 	$scope.isDP = function(label){
 		//console.log(label.name);
 		if (label.name == "DP") {return true};
 	};
 
 
-
-
+	// Set panel header to be primary if DP
 	$scope.labelValue = function (issuelabels){ 
 	//console.log(issuelabels);
 	var dp
@@ -26,30 +26,35 @@ angular.module('NoteWrangler').controller('FeatureindexController', function(Fea
 	};
 
 
-	$scope.filterDP = function(){
-	angular.forEach($scope.issues, function(value,key){
-		var id = value.id
-		//console.log($scope.issues);
-		angular.forEach(value.labels, function(value,key){
-			if (value.name == "DP") {
-			console.log(id + " " + value.name);
-		};
-		});
-	});
+	$scope.labelFilter = {};
 
+	$scope.filterDP = function(){
+	//var issue
+	//angular.forEach($scope.issues, function(value,key){
+		//issue = value.id
+		//console.log($scope.issues);
+		//angular.forEach(value.labels, function(value,key){
+		//	if (value.name == "DP") {
+		//	console.log(issue);
+		//};
+		//});
+	//});
+	//console.log ('DP');
+	$scope.labelFilter = "DP";
 	};
 
 	$scope.filterSTB = function(){
-	angular.forEach($scope.issues, function(value,key){
-		var id = value.id
+	//angular.forEach($scope.issues, function(value,key){
+	//	var id = value.id
 		//console.log($scope.issues);
-		angular.forEach(value.labels, function(value,key){
-			if (value.name == "STB") {
-			console.log(id + " " + value.name);
-		};
-		});
-	});
-
+	//	angular.forEach(value.labels, function(value,key){
+	//		if (value.name == "STB") {
+	//		console.log(id + " " + value.name);
+	//	};
+	//	});
+	//});
+	//console.log ('STB');
+	$scope.labelFilter = "STB";
 	};
 
 
