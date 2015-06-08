cce33927168c241f3bcecb88dde33a708dcde381
dave-pryce <dp@Davids-MacBook-Pro.local>
2015-06-08 15:13:00 +1000

    changed production rb to pre-compile.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-//= require angular
 //= require angular-resource
 //= require angular-route
 
