44d6613e1a208d8c94564a3379257c9991b4333e
dave-pryce <dp@Davids-MacBook-Pro.local>
2015-06-08 15:35:43 +1000

    sorting asset pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require bootstrap-sprockets
 //= require angular-resource
 //= require angular-route
+//= require_tree .
 
