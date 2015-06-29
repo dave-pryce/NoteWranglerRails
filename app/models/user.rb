57859036b2e7a6d857c8b197554d0bf12e5fc85e
dave-pryce <dp@Davids-MacBook-Pro.local>
2015-06-29 19:58:16 +1000

    playing with user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	belongs_to :note
+
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428100125) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20150629093634) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150428100125) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
-    t.text     "content"
+    t.text     "content",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
@@ -33,8 +30,8 @@ ActiveRecord::Schema.define(version: 20150428100125) do
     t.integer  "category_id"
   end
 
-  add_index "notes", ["category_id"], name: "index_notes_on_category_id", using: :btree
-  add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
+  add_index "notes", ["category_id"], name: "index_notes_on_category_id"
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -43,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150428100125) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "password_digest"
   end
 
 end
